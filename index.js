e6df227c4a438196a07b6f84ced0bea400526f63
Alex Hamilton <alex.hamilton@ou.edu>
2017-01-24 20:24:08 -0600

    Stub out the i2c module.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-var i2c = require('i2c')
+var i2c;
+
+try {
+	i2c = require('i2c')
+} catch (e) {
+	console.warn(`Module i2c could not be loaded. The functions in this library won't actually do anything until you fix this.`)
+	i2c = class {
+		write(data, callback) {
+			console.warn(`i2c#write called with arguments ${params} without loading i2c; doing nothing`)
+			return callback()
+		}
+		scan(callback) {
+			console.warn(`i2c#scan called with arguments ${params} without loading i2c; doing nothing`)
+			return callback()
+		}
+	}
+}
 
 module.exports = class {
 
@@ -8,19 +24,19 @@ module.exports = class {
 
 	setPixel(x, y, r, g, b, callback = err => { throw err }) {
 		if(x < 0 || x > 7) {
-			return callback(new Error(`The supplied x coordinate ${x} lies outside of the legal range [0, 7].`))
+			return callback(new Error(`The supplied x coordinate ${x} lies outside the legal range [0, 7].`))
 		}
 		if(y < 0 || y > 7) {
-			return callback(new Error(`The supplied y coordinate ${y} lies outside of the legal range [0, 7].`))
+			return callback(new Error(`The supplied y coordinate ${y} lies outside the legal range [0, 7].`))
 		}
 		if(r < 0 || r > 63) {
-			return callback(new Error(`The supplied red component ${r} lies outside of the legal range [0, 63].`))
+			return callback(new Error(`The supplied red component ${r} lies outside the legal range [0, 63].`))
 		}
 		if(g < 0 || g > 63) {
-			return callback(new Error(`The supplied green component ${g} lies outside of the legal range [0, 63].`))
+			return callback(new Error(`The supplied green component ${g} lies outside the legal range [0, 63].`))
 		}
 		if(b < 0 || b > 63) {
-			return callback(new Error(`The supplied blue component ${b} lies outside of the legal range [0, 63].`))
+			return callback(new Error(`The supplied blue component ${b} lies outside the legal range [0, 63].`))
 		}
 		this.wire.write([x, y, r, g, b], callback)
 	}
