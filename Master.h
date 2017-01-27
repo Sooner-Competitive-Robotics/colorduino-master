ef4a715cd070100dc411a9982f553dc69dc33c68
Alex Hamilton <alex.hamilton@ou.edu>
2017-01-27 17:20:51 -0600

    More whitespace stuff, I guess.

--- Master.h
+++ Master.h
@@ -2,9 +2,11 @@
 #define FLUFFY_KITTENS
 
 class Master {
+	
     Master(int address);
     Master(void); // Default address is 8.
     int setPixel(int x, int y, int r, int g, int b); // Returns 0 on success, -1 on failure.
+    
 };
 
 #endif // FLUFFY_KITTENS
