45e493cbbab7d76ca7f06f0c8e8da6cb2ca73d21
Alex Hamilton <alex.hamilton@ou.edu>
2017-01-28 17:21:48 -0600

    Add convenience functions for red, blue, and yellow.

--- Master.cpp
+++ Master.cpp
@@ -21,11 +21,25 @@ class Master {
       // There's apparently no way to un-join the bus, so that's cool. You should use this class as a singleton.
     }
 
-    int setPixel(int x, int y, int r, int g, int b) {
+    int setPixel(int x, int y, int r, int g, int b) { // The cash cow. The money-maker. Et cetera.
       Wire.beginTransmission(address);
       int success = Wire.write(x) && Wire.write(y) && Wire.write(r) && Wire.write(g) && Wire.write(b); // This is a little bit ugly, but it'll work.
       Wire.endTransmission(address);
       return success ? 0 : -1;
+    }
+
+    /// Convenience functions; these just call setPixel with a predefined color.
+
+    int setPixelBlue(int x, int y) {
+      return setPixel(x, y, 0, 0, 63);
+    }
+
+    int setPixelRed(int x, int y) {
+      return setPixel(x, y, 63, 0, 0);
+    }
+
+    int setPixelYellow(int x, int y) {
+      return setPixel(x, y, 63, 63, 0);
     }
 
 };
--- Master.h
+++ Master.h
@@ -5,6 +5,11 @@ class Master {
 	
     Master(int address);
     Master(void); // Default address is 8.
+    
+    int setPixelBlue(int x, int y); // Returns 0 on success, -1 on failure.
+    int setPixelRed(int x, int y); // Returns 0 on success, -1 on failure.
+    int setPixelYellow(int x, int y); // Returns 0 on success, -1 on failure.
+
     int setPixel(int x, int y, int r, int g, int b); // Returns 0 on success, -1 on failure.
     
 };
